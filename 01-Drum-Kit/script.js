586396901c5a1ecdebdef713c75b7ce22d55adfc
Stephany <stephanykleen@gmail.com>
2023-03-22 14:15:08 -0700

    Update Key-Drum exercise from deprecated keyCode property.

--- 01-Drum-Kit/script.js
+++ 01-Drum-Kit/script.js
@@ -1,22 +1,21 @@
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio) {
-    return; //stops function from running all together
-  }
-  audio.currentTime = 0; //rewind to the start.
-  audio.play();
-  key.classList.add("playing");
-}
+window.addEventListener('keydown', playSound)
+  
 
-function removeTransition(e) {
-  if (e.propertyName !== "transform") {
-    return;
-  } // skip it if it's not a transform
-  this.classList.remove("playing");
-}
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) =>
-  key.addEventListener("transitionend", removeTransition)
-);
-window.addEventListener("keydown", playSound);
+function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    if(!audio){return}
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add('playing')
+    console.log(key)
+  }
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform'){
+      return}
+    this.classList.remove('playing')
+  }
+  
+  const keys = document.querySelectorAll('.key')
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
